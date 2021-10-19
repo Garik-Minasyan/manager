aac9748d42654a7de908a5cd6d0542cec4d0e59c
Garik Minasyan <garominas96@gmail.com>
2021-10-19 19:51:10 +0400

    ref

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-// import Home from "./components/Home";
 import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
 import Breadcrumbs from "./components/Breadcrumbs";
 import Header from "./components/Header.js";
@@ -6,7 +5,6 @@ import './App.css';
 import CreateFolder from "./components/createFolder/CreateFolder.js";
 import CreateFile from './components/createFile/CreateFile';
 import { useSelector } from 'react-redux';
-import Main from './pages/Main';
 import Folder from "./components/createFolder/Folder";
 // import File from './components/createFile/File';
 
@@ -19,22 +17,9 @@ export default function App() {
         {isOpened ? <CreateFolder /> : <CreateFile />}
         <Breadcrumbs />
         <Switch>
-          {/*           
-          <Route exact path="/">
-            <Main />
-          </Route>
-          <Route exact path="/fo">
-            <h1>foooooooooooooooo</h1>
-          </Route>
-          <Route exact path="/fo/oo">
-            <h1>rrrrrrrrrrrrrrrrrrrrr</h1>
-          </Route> */}
-
-
           <Route exact path="/:folder">
             <Folder />
           </Route>
-
         </Switch>
       </Router>
     </div>
--- src/components/Home.js
+++
@@ -1,9 +0,0 @@
-const Home = () => {
-    return (
-        <>
-            <h1>File Manajer</h1>
-        </>
-    );
-};
-
-export default Home;
